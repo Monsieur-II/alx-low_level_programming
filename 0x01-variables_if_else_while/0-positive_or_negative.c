1885db72d584696f4d316a290c46450bc1e33462
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-17 08:54:36 -0800

    positive-compiled

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,7 +12,7 @@ int main(void)
 {
 	int n;
 
-	srand(time(0);
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
