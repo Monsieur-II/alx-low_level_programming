819a48cdb7c493aa2a69cf0f22f09f47b354f26b
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-21 20:38:09 -0800

    added files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: the number
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digi;
+
+	last_digi = n % 10;
+	if (last_digi < 0)
+	{
+		last_digi *= -1;
+		_putchar(last_digi + 48);
+		return (last_digi);
+	}
+	else
+	{
+		_putchar(last_digi + 48);
+		return (last_digi);
+	}
+}
