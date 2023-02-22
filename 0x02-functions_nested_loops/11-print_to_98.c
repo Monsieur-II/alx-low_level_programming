762456971276909fabe4cff5ec97e2bcd54af133
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-21 21:37:14 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,29 +9,21 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n == 98)
-		printf("%d", n);
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
 	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		while (n > 98)
 		{
-			if (i == 98)
-				printf("%d", i);
-			else
-				printf("%d ", i);
+			printf("%d, ", n);
+			n--;
 		}
 	}
-	else if (n < 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			if (i == 98)
-				printf("%d", i);
-			else
-				printf("%d ", i);
-		}
-	}
-	printf("\n");
+	printf("98\n");
 }
