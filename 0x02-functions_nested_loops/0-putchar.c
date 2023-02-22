780339578ca8a3f694dc06c30992f864cc171a7b
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-21 19:24:33 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: print '_putchar' followed by a line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+/**
+ * puthcar - prints a character to standard output
+ *
+ * Return: The character
+ */
+int putchar(char c);
+
