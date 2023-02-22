65b57493a7b27b1628be38af815a9ded06e4e617
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-21 20:57:24 -0800

    9

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of JB,
+ * starting from 00:00 to 23:59
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = m = 0;
+	while (h < 24)
+	{
+		while (m < 60)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+			m++;
+		}
+		m = 0;
+		h++;
+	}
+}
