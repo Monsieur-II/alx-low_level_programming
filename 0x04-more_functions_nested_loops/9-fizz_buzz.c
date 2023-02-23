4361e8881f0ef5137591a995a9b69470c80843c4
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-02-23 10:55:21 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints fizzbuzz, fizz, buzz or the number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+		{
+			printf("Buzz\n");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
