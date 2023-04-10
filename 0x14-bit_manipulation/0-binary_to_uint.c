b5c4d4c6948f2db20093c2d8586fc9fadef01021
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-04-10 16:22:01 -0700

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - prints length of string pointed by *s
+ * @s: pointer to string
+ *
+ * Return: 0;
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
+
+/**
+ * binary_to_uint - converts binary string to an unsigned int
+ * @b:  pointer to the string
+ *
+ * Return: converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = _strlen(b);
+	unsigned int decimal = 0;
+	int i, j, digit;
+
+	for (i = len - 1, j = 0; i >= 0; i--, j++)
+	{
+		if ((b[i] != '0' && b[i] != '1') || (b == NULL))
+			return (0);
+		digit = b[i] - '0';
+		decimal = decimal + (digit * (2 ^ j));
+	}
+
+	return (decimal);
+}
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+int _strlen(char *s);
+unsigned int binary_to_uint(const char *b);
+#endif
