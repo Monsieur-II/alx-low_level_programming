70331e517794db1516a3418eda041d57cc278088
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-04-16 10:38:13 -0700

    eror

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,45 @@
 #include "main.h"
+
 /**
- * create_file -creates an array of chars, and initializes
+ * create_file - creates a file
  *
- * @text_content: is a NULL terminated string to write to the file
- * @filename: is the name of the file to create
+ * @filename: name of file
+ * @text_content: content to write into file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, len = 0;
+	ssize_t w, i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	if (fd == -1)
 		return (-1);
 
-	close(o);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
+	while (text_content[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	w = write(fd, text_content, len);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
 	return (1);
 }
