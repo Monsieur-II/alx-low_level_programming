69c5e3c9fe8d8f02cb252f096086919a71b6db1f
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-04-19 16:37:46 -0700

    heh

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+#endif /*MAIN_HEADER*/
+
