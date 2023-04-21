9c6e289addee8564e39f382d80960c6fb4b56209
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-04-21 14:44:58 +0000

    getline

--- shell_project/getline.c
+++ shell_project/getline.c
@@ -1,59 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
+/**
+ * _getline - reads a line from a stream stores it in a dynamically allocated buffer.
+ * @lineptr: the address of the buffer
+ * @n: address pointing to the size of the buffer
+ * @stream: the stream to read from
+ *
+ * Return: the number of bytes read from stream, -1 on error
+ */
+
+#define INITIAL_BUFFER_SIZE 1024
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-    ssize_t pos = 0;
-    char c;
+    static size_t buffer_size = INITIAL_BUFFER_SIZE;
+    ssize_t num_read = 0;
+    char *buf = *lineptr;
 
-    // get characters
-    while ((c = getc(stream)) != EOF)
+    if (buf = NULL || *n < buffer_size)
     {
-        // if buffersize is not enough increase it. *n is the size of the buffer that lineptr points to
-        if (pos + 1 >= *n)
-        {
-            *n += 25;
-
-            *lineptr = realloc(*lineptr, *n); // if lineptr is null realloc just allocates the new buffersize to it
-            if (*lineptr == NULL)
-            {
-                perror("Unable to allocate memory");
-                exit(EXIT_FAILURE);
-            }
-        }
-        (*lineptr)[pos++] = c;
-        if (c == '\n')
-            break; // break upon newline
+        buffer_size = INITIAL_BUFFER_SIZE; // resetting buffersize's value regardless of previous call
+        if ((buf = realloc(buf, buffer_size)) == NULL)
+            return (-1);
     }
 
-    // If no character was read, return -1
-    if (pos == 0)
+    *lineptr = buf;
+    *n = buffer_size; // update buffer and its after realloc
+
+    while ((num_read = read(stream, buf, buffer_size - 1)) > 0) // num_read > means EOF not reached so we loop
+    {
+        if (buf[num_read - 1] == \n) // break upon newline and terminate buffer with '\0'
+        {
+            buf[num_read] = '\0';
+            *lineptr = buf;
+            *n = buffer_size;
+            break;
+        }
+
+        buffer_size *= 2;
+        if ((buf = realloc(buf, buffer_size)) == NULL)
+            return (-1);
+    }
+
+    if (num_read == -1)
         return (-1);
 
-    (*lineptr)[pos] = '\0'; // terminating the string
-
-    return (pos - 1);
-}
-
-/*int main(void)
-{
-    char *lineptr = NULL;
-    size_t n = 0;
-    ssize_t nread;*/
-
-    /*lineptr = malloc(n);
-    if (lineptr == NULL)
+    else if (num_read == 0) // upon EOF, update buffer and its size after realloc in loop, then terminate it
     {
-        fprintf(stderr, "Unable to allocate memory\n");
-        exit(EXIT_FAILURE);
-    }*/
-
-    /*nread = _getline(&lineptr, &n, stdin);
-    if (nread != -1)
-        printf("%s\n", lineptr);
-
-    free(lineptr);
-
-    return (0);
-}*/
+        *lineptr = buf;
+        *n = buffer_size;
+        lineptr[num_read] = '\0';
+    }
+}
