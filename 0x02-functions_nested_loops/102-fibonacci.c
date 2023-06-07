9cedd1a81b71d9fe91b9aacb2f7f649eccaa58d7
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-07 03:15:49 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	signed long int a, b, fibo;
+	int i;
+
+	a = 1;
+	b = 2;
+
+	printf("%ld, %ld, ", a, b);
+
+	for (i = 0; i < 48; i++)
+	{
+		fibo = b + a;
+		printf("%ld", fibo);
+
+		if (i != 47)
+			printf(", ");
+		else
+			printf("\n");
+
+		a = b;
+		b = fibo;
+	}
+	return (0);
+}
