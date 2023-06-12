f72a068e52a4c51497a63c01de918725042a7488
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-12 13:41:06 -0700

    ei

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,28 +17,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL || (*head == NULL && idx != 0))
 		return (NULL);
 
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	temp = *head;
 	i = 0;
 
-	while (i < idx)
+	while (i < idx && temp)
 	{
-		temp = temp->next;
-
-		if (temp == NULL)
+		if (i == idx - 1)
 		{
-			if (i == idx - 1)
-			{
-				i++;
-				continue;
-			}
-			else
-				return (NULL);
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
 		}
-
+		else
+			temp = temp->next;
 		i++;
 	}
 
-	new_node = add_nodeint(&temp, n);
-
-	return (new_node);
+	free(new_node);
+	return (NULL);
 }
