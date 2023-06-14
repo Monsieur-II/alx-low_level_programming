b202df96a7648b75326a5e3f316ebc3d75b833ae
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-06-14 04:57:25 -0700

    update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,33 +8,43 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	size_t len = 0, ctrl_len;
+	listint_t *current, *temp, *control;
 
 	if (!h || !*h)
-		return (0);
-
-	while (*h)
+		return (len);
+	current = *h;
+	while (current)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		len++;
+		temp = current->next;
+		free(current);
+		current = temp;
+
+		control = *h;
+		ctrl_len = 0;
+		while (ctrl_len < len)
 		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
+			if (current == control)
+			{
+				*h = NULL;
+				return (len);
+			}
+			control = control->next;
+			ctrl_len++;
 		}
 	}
-
 	*h = NULL;
-
 	return (len);
 }
+
+/*
+ * This is a revised implementation. Most alx solutions to this task,
+ * used pointer arithmetic to check if a node points to a previous node.
+ * This gives accurate results only if all new nodes were added at the
+ * beginning or at the end of the linked list.
+ * What if a first node was added, then the second node was pushed at the end,
+ * then all other nodes were pushed at the start? second node that was added
+ * is now the last and what if it is made to point to say the 1st node?
+ * Will pointer arithmetic work for a free safe?
+ */
