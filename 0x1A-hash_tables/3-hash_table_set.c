b48cdaafda7e8cc0bb289ff0da65f7f1de407b93
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-07-07 17:41:26 -0700

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!key || !ht)
 		return (0);
-	
+
 	index = key_index((unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
