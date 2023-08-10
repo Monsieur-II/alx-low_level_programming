f1800bba4026eb1b0e18f784644d50954af616d7
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-08-10 22:06:55 +0000

    more malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * *_strncpy - copies at most n bytes from src to dest
+ * @dest: the buffer storing the string copy
+ * @src: source string
+ * @n: max number of bytes to copy from src
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
  * word_count - counts the number of words in string
  * @str: the string
  *
@@ -63,7 +87,7 @@ char **strtow(char *str)
 				break;
 		}
 		start = &(str[j]);
-		while (str[j] != ' ')
+		while (str[j] != ' ' && str[j])
 		{
 			len++;
 			j++;
@@ -71,9 +95,8 @@ char **strtow(char *str)
 		new[i] = malloc(sizeof(char) * (len + 1));
 		if (!new[i])
 			return (NULL);
-		for (k = 0; k < len; k++)
-			new[i][k] = start[k];
-		new[i][k] = '\0';
+		_strncpy(new[i], start, len);
+		new[i][len] = '\0';
 	}
 	new[words] = NULL;
 
