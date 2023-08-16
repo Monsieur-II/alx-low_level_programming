55b544fb19753b0bef9df739b5dc1c0e4bf67360
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-08-16 21:01:32 +0000

    function pointers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,44 +8,43 @@
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int i, j, k;
 
-	if (n >= 0 && n <= 14)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (x = 0; x <= n; x++)
+		for (j = 0; j <= n; j++)
 		{
-			for (y = 0; y <= n; y++)
+			k = j * i;
+			if (j == 0)
+				_putchar(k + 48);
+			else if (k < 10)
 			{
-				z = x * y;
-				if (x > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + 48);
 			}
-			_putchar('\n');
+			else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + 48);
+				_putchar(((k / 10) % 10) + 48);
+				_putchar((k % 10) + 48);
+			}
 		}
+		_putchar('\n');
 	}
 }
