12e1687582866d8648ea11e52aae1b5da3a8122c
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-08-28 20:13:22 +0000

    betty

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -23,27 +23,3 @@ listint_t *reverse_listint(listint_t **head)
 
 	return (*head);
 }
-
-/*
-my_listint_t *reverse_listint(listint_t **head)
-{
-    listint_t *prev = NULL;
-    listint_t *next = NULL;
-
-    if (!head || !*head)
-        return (NULL);
-
-    next = (*head)->next;
-
-    while (next)
-    {
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
-        next = next->next;
-        (*head)->next = prev;
-    }
-
-    return (*head);
-}
-*/
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,1 +1,23 @@
 readme file
+my_listint_t *reverse_listint(listint_t \*\*head)
+{
+listint_t *prev = NULL;
+listint_t \*next = NULL;
+
+    if (!head || !*head)
+    	return (NULL);
+
+    next = (*head)->next;
+
+    while (next)
+    {
+    	(*head)->next = prev;
+    	prev = *head;
+    	*head = next;
+    	next = next->next;
+    	(*head)->next = prev;
+    }
+
+    return (*head);
+
+}
