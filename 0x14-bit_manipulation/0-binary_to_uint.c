485d7b8c0d095cbd9540e4f50fb3de938a029ae0
Evans Nyarko <evans.nyarko39@yahoo.com>
2023-08-31 22:07:36 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary string to uns. int
+ * @b: the binary string
+ *
+ * Return: the converted num, 0 if string is
+ * not binary and 1 if b is null
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int power, len;
+	unsigned int dec = 0;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (len--, power = 1; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			dec += power;
+	}
+
+	return (dec);
+}
